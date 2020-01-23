655c31aa6052adf0ceb48e0e706cb9987b3cff03
PaperPlane01 <dagfortnewage@gmail.com>
2020-01-23 14:45:46 +0600

    refactor: (23.01.20) Changed stylink of links in NavigationMenu component

--- front-end/src/Navigation/components/NavigationMenu.tsx
+++ front-end/src/Navigation/components/NavigationMenu.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from "react";
 import {inject} from "mobx-react";
-import {List, ListItemIcon, ListItemText, MenuItem} from "@material-ui/core";
+import {List, ListItemIcon, ListItemText, MenuItem, createStyles, makeStyles} from "@material-ui/core";
 import HistoryIcon from "@material-ui/icons/History"
 import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
@@ -20,79 +20,78 @@ interface NavigationMenuOwnProps {
 
 type NavigationMenuProps = NavigationMenuMobxProps & NavigationMenuOwnProps;
 
+const useStyles = makeStyles(() => createStyles({
+    undecoratedLink: {
+        textDecoration: "none",
+        color: "inherit"
+    }
+}));
+
 const _DataValidatorNavigationMenu: FunctionComponent<NavigationMenuProps> = ({
     store,
     onItemClick
-}) => (
-    <List>
-        <Link store={store}
-              view={Routes.home}
-              style={{
-                  textDecoration: 'none',
-                  color: 'inherit'
-              }}
-        >
-            <MenuItem onClick={() => onItemClick && onItemClick()}>
-                <ListItemIcon>
-                    <HistoryIcon/>
-                </ListItemIcon>
-                <ListItemText>
-                    Data purchases
-                </ListItemText>
-            </MenuItem>
-        </Link>
-        <Link store={store}
-              view={Routes.files}
-              style={{
-                  textDecoration: 'none',
-                  color: 'inherit'
-              }}
-        >
-            <MenuItem onClick={() => onItemClick && onItemClick()}>
-                <ListItemIcon>
-                    <InsertDriveFileIcon/>
-                </ListItemIcon>
-                <ListItemText>
-                    Explore files
-                </ListItemText>
-            </MenuItem>
-        </Link>
-        <Link store={store}
-              view={Routes.dataMartRegistration}
-              style={{
-                  textDecoration: 'none',
-                  color: 'inherit'
-              }}
-        >
-            <MenuItem onClick={() => onItemClick && onItemClick()}>
-                <ListItemIcon>
-                    <AccountCircleIcon/>
-                </ListItemIcon>
-                <ListItemText>
-                    Register
-                </ListItemText>
-            </MenuItem>
-        </Link>
-        {process.env.REACT_APP_KIBANA_DASHBOARD_URL && (
-            <a href={`${process.env.REACT_APP_KIBANA_DASHBOARD_URL}`}
-               target="_blank"
-               style={{
-                   textDecoration: 'none',
-                   color: 'inherit'
-               }}
+}) => {
+    const classes = useStyles();
+
+    return (
+        <List>
+            <Link store={store}
+                  view={Routes.home}
+                  className={classes.undecoratedLink}
             >
-                <MenuItem>
+                <MenuItem onClick={() => onItemClick && onItemClick()}>
                     <ListItemIcon>
-                        <DashboardIcon/>
+                        <HistoryIcon/>
                     </ListItemIcon>
                     <ListItemText>
-                        Kibana Dashboard
+                        Data purchases
                     </ListItemText>
                 </MenuItem>
-            </a>
-        )}
-    </List>
-);
+            </Link>
+            <Link store={store}
+                  view={Routes.files}
+                  className={classes.undecoratedLink}
+            >
+                <MenuItem onClick={() => onItemClick && onItemClick()}>
+                    <ListItemIcon>
+                        <InsertDriveFileIcon/>
+                    </ListItemIcon>
+                    <ListItemText>
+                        Explore files
+                    </ListItemText>
+                </MenuItem>
+            </Link>
+            <Link store={store}
+                  view={Routes.dataMartRegistration}
+                  className={classes.undecoratedLink}
+            >
+                <MenuItem onClick={() => onItemClick && onItemClick()}>
+                    <ListItemIcon>
+                        <AccountCircleIcon/>
+                    </ListItemIcon>
+                    <ListItemText>
+                        Register
+                    </ListItemText>
+                </MenuItem>
+            </Link>
+            {process.env.REACT_APP_KIBANA_DASHBOARD_URL && (
+                <a href={`${process.env.REACT_APP_KIBANA_DASHBOARD_URL}`}
+                   target="_blank"
+                   className={classes.undecoratedLink}
+                >
+                    <MenuItem>
+                        <ListItemIcon>
+                            <DashboardIcon/>
+                        </ListItemIcon>
+                        <ListItemText>
+                            Kibana Dashboard
+                        </ListItemText>
+                    </MenuItem>
+                </a>
+            )}
+        </List>
+    )
+};
 
 const mapMobxToProps = (state: IAppState): NavigationMenuMobxProps => ({
     store: state.store
