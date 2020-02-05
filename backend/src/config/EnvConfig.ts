59f9139bc11a6ff5efc01967dc06c3ba116519a6
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-05 17:40:43 +0600

    fix: (05.02.20) Removed unused environmental variables from EnvConfig class

--- backend/src/config/EnvConfig.ts
+++ backend/src/config/EnvConfig.ts
@@ -2,13 +2,7 @@ import {Env} from "env-decorator";
 
 export class EnvConfig {
     @Env({required: true, type: "string"})
-    SERVICE_NODE_API_URL: string;
-
-    @Env({required: true, type: "string"})
-    SERVICE_NODE_ACCOUNT_ADDRESS: string;
-
-    @Env({required: true, type: "string"})
-    ELASTICSEARCH_HOST_URL;
+    ELASTICSEARCH_HOST_URL: string;
 
     @Env({required: true, type: "number"})
     PORT: number;
