9dac18950fcfd0970713596c422a2a36fb449dd1
PaperPlane01 <dagfortnewage@gmail.com>
2020-06-24 14:45:48 +0600

    fix: (24.04.20) Fixed files schema

--- backend/src/elasticsearch/schema/files-schema.ts
+++ backend/src/elasticsearch/schema/files-schema.ts
@@ -1,58 +1,136 @@
 export const filesSchema = {
     _doc: {
         properties: {
-            id: {
-                type: "keyword",
-                index: true
-            },
-            name: {
-                type: "text",
-                index: true
-            },
-            metadata: {
-                type: "nested",
-                // index: true,
-                properties: {
-                    briefDescription: {
-                        type: "text",
-                        index: true
-                    },
-                    fullDescription: {
-                        type: "text",
-                        index: true
-                    },
-                    userComment: {
-                        type: "text",
-                        index: true
-                    },
-                    author: {
-                        type: "text",
-                        index: true
-                    },
-                    hashTags: {
-                        type: "keyword"
+            properties: {
+                dataOwner: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                dataValidator: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                extension: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                id: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                metadata: {
+                    properties: {
+                        author: {
+                            type: "text",
+                            fields: {
+                                keyword: {
+                                    type: "keyword",
+                                    ignore_above: 256
+                                }
+                            }
+                        },
+                        briefDescription: {
+                            type: "text",
+                            fields: {
+                                keyword: {
+                                    type: "keyword",
+                                    ignore_above: 256
+                                }
+                            }
+                        },
+                        fullDescription: {
+                            type: "text",
+                            fields: {
+                                keyword: {
+                                    type: "keyword",
+                                    ignore_above: 256
+                                }
+                            }
+                        },
+                        hashTags: {
+                            type: "text",
+                            fields: {
+                                keyword: {
+                                    type: "keyword",
+                                    ignore_above: 256
+                                }
+                            }
+                        },
+                        userComment: {
+                            type: "text",
+                            fields: {
+                                keyword: {
+                                    type: "keyword",
+                                    ignore_above: 256
+                                }
+                            }
+                        }
+                    }
+                },
+                mimeType: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                name: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                price: {
+                    type: "long"
+                },
+                savedAt: {
+                    type: "long"
+                },
+                serviceNode: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
+                    }
+                },
+                size: {
+                    type: "text",
+                    fields: {
+                        keyword: {
+                            type: "keyword",
+                            ignore_above: 256
+                        }
                     }
                 }
-            },
-            dataValidator: {
-                type: "keyword",
-                index: true
-            },
-            extension: {
-                type: "keyword",
-                index: true
-            },
-            mimeType: {
-                type: "keyword",
-                index: true
-            },
-            price: {
-                type: "double",
-                index: false
-            },
-            savedAt: {
-                type: "long"
             }
+
         }
     }
 };
