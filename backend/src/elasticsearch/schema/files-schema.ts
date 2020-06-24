a8ff8e6a7b1f6d214adc1ea2e135efe14bebff78
PaperPlane01 <dagfortnewage@gmail.com>
2020-06-24 14:53:22 +0600

    fix: (24.06.20) Fixed files mapping

--- backend/src/elasticsearch/schema/files-schema.ts
+++ backend/src/elasticsearch/schema/files-schema.ts
@@ -1,136 +1,133 @@
 export const filesSchema = {
     _doc: {
         properties: {
-            properties: {
-                dataOwner: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
-                        }
+            dataOwner: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
                     }
-                },
-                dataValidator: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
-                        }
+                }
+            },
+            dataValidator: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
                     }
-                },
-                extension: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
-                        }
+                }
+            },
+            extension: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
                     }
-                },
-                id: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
-                        }
+                }
+            },
+            id: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
                     }
-                },
-                metadata: {
-                    properties: {
-                        author: {
-                            type: "text",
-                            fields: {
-                                keyword: {
-                                    type: "keyword",
-                                    ignore_above: 256
-                                }
-                            }
-                        },
-                        briefDescription: {
-                            type: "text",
-                            fields: {
-                                keyword: {
-                                    type: "keyword",
-                                    ignore_above: 256
-                                }
-                            }
-                        },
-                        fullDescription: {
-                            type: "text",
-                            fields: {
-                                keyword: {
-                                    type: "keyword",
-                                    ignore_above: 256
-                                }
-                            }
-                        },
-                        hashTags: {
-                            type: "text",
-                            fields: {
-                                keyword: {
-                                    type: "keyword",
-                                    ignore_above: 256
-                                }
-                            }
-                        },
-                        userComment: {
-                            type: "text",
-                            fields: {
-                                keyword: {
-                                    type: "keyword",
-                                    ignore_above: 256
-                                }
+                }
+            },
+            metadata: {
+                properties: {
+                    author: {
+                        type: "text",
+                        fields: {
+                            keyword: {
+                                type: "keyword",
+                                ignore_above: 256
                             }
                         }
-                    }
-                },
-                mimeType: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
+                    },
+                    briefDescription: {
+                        type: "text",
+                        fields: {
+                            keyword: {
+                                type: "keyword",
+                                ignore_above: 256
+                            }
                         }
-                    }
-                },
-                name: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
+                    },
+                    fullDescription: {
+                        type: "text",
+                        fields: {
+                            keyword: {
+                                type: "keyword",
+                                ignore_above: 256
+                            }
                         }
-                    }
-                },
-                price: {
-                    type: "long"
-                },
-                savedAt: {
-                    type: "long"
-                },
-                serviceNode: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
+                    },
+                    hashTags: {
+                        type: "text",
+                        fields: {
+                            keyword: {
+                                type: "keyword",
+                                ignore_above: 256
+                            }
                         }
-                    }
-                },
-                size: {
-                    type: "text",
-                    fields: {
-                        keyword: {
-                            type: "keyword",
-                            ignore_above: 256
+                    },
+                    userComment: {
+                        type: "text",
+                        fields: {
+                            keyword: {
+                                type: "keyword",
+                                ignore_above: 256
+                            }
                         }
                     }
                 }
+            },
+            mimeType: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
+                    }
+                }
+            },
+            name: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
+                    }
+                }
+            },
+            price: {
+                type: "long"
+            },
+            savedAt: {
+                type: "long"
+            },
+            serviceNode: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
+                    }
+                }
+            },
+            size: {
+                type: "text",
+                fields: {
+                    keyword: {
+                        type: "keyword",
+                        ignore_above: 256
+                    }
+                }
             }
-
         }
     }
 };
